4fe950245ff234a7039ac55998b4780bc957f7e1
Navneet <navneetsinha0307@gmail.com>
2026-01-01 22:05:30 +0530

    first

---
+++ class0/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Hi guys,</h1>
+      <h2>Kaise ho saare?</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ class1-task/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl p-15 font-bold text-amber-200 bg-amber-600 
+      rounded-4xl m-20 w-100 '>Hari bolo bhai!</h1>
+    </div>
+  )
+}
+
+export default App
