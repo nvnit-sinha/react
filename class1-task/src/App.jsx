cf290ec7151c505184c21e1ae39ca9a702d30676
Navneet <navneetsinha0307@gmail.com>
2026-01-02 23:49:39 +0530

    Ram

--- class1-task/src/App.jsx
+++ class1-task/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     <div>
       <h1 className='text-3xl p-15 font-bold text-amber-200 bg-amber-600 
       rounded-4xl m-20 w-100 '>Hari bolo bhai!</h1>
+      <h2 className='text-2xl p-15 font-thin text-cyan-700 bg-emerald-400 w-100 m-20 rounded-2xl'>Jai shree Ram</h2>
     </div>
   )
 }
