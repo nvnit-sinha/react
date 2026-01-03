9a949cc046389e4f4e1c8575c4c4df00e4dd52d3
Navneet <navneetsinha0307@gmail.com>
2026-01-03 16:51:18 +0530

    project

--- class2/src/App.jsx
+++ class2/src/App.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import Card from './components/Card'
 
 const App = () => {
-  
   return (
-    <div>
-      <Card text='Buy Now' />
-      <Card text='Explore More' />
+    <div className='h-screen bg-black flex justify-evenly items-center'>
+      <Card name='gurl' rating={4.4} img="https://images.pexels.com/photos/3541331/pexels-photo-3541331.jpeg" earning={45} rate={50} slogan="I'm a Brand designer who focuses on clarity & emotional connection." />
+      <Card name='boy' rating={4.9} img="https://images.pexels.com/photos/3752652/pexels-photo-3752652.jpeg" earning={85} rate={80} slogan="I'm a UX designer who focuses on exquisite design & top-notch performance." />
     </div>
   )
 }
--- class2/src/components/Card.jsx
+++ class2/src/components/Card.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 const Card = (props) => {
   return (
-    <div className='bg-emerald-500 w-fit p-4 m-5 rounded-md'>
-      <h1 className='text-2xl font-bold'>{props.text}</h1>
+    <div className='bg-gray-300 h-111 w-70 rounded-3xl relative'>
+
+        <img className='h-111 w-100 p-2 object-cover rounded-3xl' src={props.img} alt="" />
+
+        <div className='absolute bottom-1.5 p-4 flex flex-col gap-2 bg-transparent' >
+            <h1 className='text-white px-3'>{props.name} <i class="ri-verified-badge-fill text-blue-800"></i></h1>
+
+            <p className='text-white px-3 leading-5'>{props.slogan}</p>
+
+            <div className='text-white flex justify-between items-center px-3 leading-5'>
+                <div>
+                <i class="ri-star-fill text-amber-300"></i>
+                <span> {props.rating}</span>
+                <h3>Rating</h3>
+                </div>
+
+                <div>
+                    <h3>${props.earning}k+</h3>
+                    <h3>Earned</h3>
+                </div>
+
+                <div>
+                    <h3>${props.rate}/hr</h3>
+                    <h3>Rate</h3>
+                </div>
+            </div>
+            <div className='flex gap-8 mt-1'>
+                    <div className='flex gap-1  bg-white text-gray-600 w-fit rounded-3xl px-10 py-0.5'>
+                    <i class="ri-mail-line"></i>
+                    <span>Get in Touch</span>
+                    </div>
+                    <h3 className='rounded-xl px-1 bg-gray-400'><i class="ri-bookmark-line text-white"></i></h3>
+            </div>
+
+        </div>
     </div>
   )
 }
