e8942905099f4761e77cc89c725a3e1709996bd4
Navneet <navneetsinha0307@gmail.com>
2026-01-04 23:03:55 +0530

    useState

---
+++ class3/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+
+  const [first, setfirst] = useState(second)
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
