a2476f7f73ab0f61e8c9beb64c8a05e37f3e623a
Navneet <navneetsinha0307@gmail.com>
2026-01-07 21:42:46 +0530

    preventDefault

--- class4/src/App.jsx
+++ class4/src/App.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
 
   const [title, setTitle] = useState('')
-  const [allUsers, setAllUsers] = useState(['aman'])
+  const [allUsers, setAllUsers] = useState([])
 
-  const submitHandler = (e) => {
-    e.preventDefault()
+  function submitHandler(e){
+    e.preventDefault();
+
     const oldUsers = [...allUsers]
     oldUsers.push(title)
 
+    setAllUsers(oldUsers)
+
+    setTitle('')
   }
 
   return (
-    <div className='h-screen w-screen bg-white'>
-      <form onSubmit={(e)=> {
-        submitHandler(e)
-      }}>
-        <input type="text" 
-          placeholder='enter your name'
-          value={title}
-          required
-          onChange = {(e)=>{
-            setTitle(e.target.value)
-          }}
-        />
-        <button type="submit">Submit</button>
+    <div className='h-screen w-screen bg-black text-white p-2'>
+      <h1 className='text-4xl mb-4 px-1'>hey bruh!</h1>
+      <form 
+        onClick={(e)=>{
+          submitHandler(e)
+        }}
+        className='flex flex-col'>
+        <input 
+        type="text" 
+        className='w-fit px-2 py-1'
+        placeholder='Enter your name' 
+        value={title}
+        required
+        onChange={(e)=>{
+          setTitle(e.target.value)
+        }}
+         />
+        <button className='bg-gray-600 px-2 w-fit mt-3 rounded py-1'>Submit</button>
       </form>
+      {
+        allUsers.map((elem,idx)=>{
+          return <h2 key={idx}>{elem}</h2>
+        })
+      }
     </div>
   )
 }
